484267a61475514351e8137013568d98edb02f72
ahmad <saleh_ahmad@outlook.com>
2020-02-19 16:58:15 +0200

    delete unused variable

--- src/main/java/importFile/ImportFile.java
+++ src/main/java/importFile/ImportFile.java
@@ -4,17 +4,13 @@ import com.sun.xml.internal.ws.api.policy.ModelGenerator;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
 
-import java.io.*;
-import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ImportFile {
-  private final String path;
   private Instances dataset;
 
   public ImportFile(String path) {
-    this.path = path;
     try {
       dataset = ConverterUtils.DataSource.read(path);
       if (dataset.classIndex() == -1) {
